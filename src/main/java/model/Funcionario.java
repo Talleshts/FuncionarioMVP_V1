17467c77e7cde52e43c23cc76afd69a52c35ef73
Arrr-az <arthur_santosazv@outlook.com>
2023-11-01 22:47:58 -0300

    Funcionario.java + FuncionarioCollection.java

--- src/main/java/model/Funcionario.java
+++ src/main/java/model/Funcionario.java
@@ -4,10 +4,53 @@
  */
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
- * @author talle
+ * @author Arthu
  */
 public class Funcionario {
+    private long id;
+    private String nome;
+    private String cargo;
+    private LocalDate dataAdmissao;
+    private double salario;
     
+    public Funcionario(long id, String nome, String cargo, LocalDate dataAdmissao, double salario){
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.dataAdmissao = dataAdmissao;
+        this.salario = salario;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
+
--- src/main/java/model/FuncionarioCollection.java
+++ src/main/java/model/FuncionarioCollection.java
@@ -4,10 +4,68 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author talle
+ * @author Arthu
  */
 public class FuncionarioCollection {
     
+    private static FuncionarioCollection funcionarioCollection;
+    
+    private ArrayList<Funcionario> funcionarios;
+    
+    private FuncionarioCollection(){
+        funcionarios = new ArrayList<>();
+    }
+    
+    public static FuncionarioCollection getInstancia(){
+        
+        if(funcionarioCollection == null){
+            funcionarioCollection = new FuncionarioCollection();
+        }
+        
+        return funcionarioCollection;
+    }
+    
+    public void adicionarFuncionario(Funcionario funcionario){
+        funcionarios.add(funcionario);
+    }
+    
+    public void removerFuncionario(long id){
+        
+        for(Funcionario funcionario : funcionarios){
+            
+            if(funcionario.getId() == id){
+                funcionarios.remove(funcionario);
+                return;
+            }
+        }
+    }
+    
+    public void listarFuncionario(long id){
+        
+        for(Funcionario funcionario : funcionarios){
+            
+            if(funcionario.getId() == id){
+                System.out.println(funcionario.getNome());
+                return;
+                /*
+                    Na verdaaaaade, nós vamos tirar esse println daqui e adicionar
+                    no JTextField(?) a Stringzona com todos os campos de funcionario
+                */
+            }
+        }
+    }
+    
+    /*
+        Tallin, vi que no seu diagrama de classe tinha um método "atualizar",
+        mas acho que não precisa, olhando o requisito do trabalho.
+    
+        Além disso, provavelmente teríamos que criar um novo botão de atualizar,
+        uma nova tela de atualizar... etc.
+    */
+    
 }
+
